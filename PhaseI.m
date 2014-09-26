b4d00acb37219f73bd4852a528bf6756f33a5e82
zamiji3 <zamiji3@gatech.edu>
2014-09-26 15:14:17 -0400

    Update Phase1.m

--- PhaseI.m
+++ PhaseI.m
@@ -3,25 +3,24 @@ clc
 
 % Simulation Parameters
 simtime=100; %ms
-dt=.04; %ms time step
+dt=1; %ms time step
 t=0:dt:simtime; %ms time array
-
 
 % Constants
 gK=36; %mS/cm^2
 gNa=120; %mS/cm^2
 gL=.3; %mS/cm^2
 EK=-12; %mV
-ENa=10.6; %mV
+ENa=115; %mV
 EL=10.6; %mV
 Vrest=-70; %mV
 Cm=1.0; %uF/cm^2
-Vm(1)=-70; %mV initial membrane voltage
+Vm(1)=Vrest; %mV initial membrane voltage
 I=0; %injection current
 
 % Equations
 
-% Gating variables
+% Initial gating variables
 alpha_m=.1*((25-Vm(1))/(exp((25-Vm(1))/10)-1));
 beta_m=4*exp(-Vm(1)/18);
 alpha_n=.01*((10-Vm(1))/(exp((10-Vm(1))/10)-1));
@@ -29,43 +28,42 @@ beta_n=.125*exp(-Vm(1)/80);
 alpha_h=.07*exp(-Vm(1)/20);
 beta_h=1./(exp((30-Vm(1))/10)+1);
 
+% Initial m/n/h values
 m(1)=alpha_m./(alpha_m+beta_m);
 n(1)=alpha_n./(alpha_n+beta_n);
-h(1)=alpha_h./(alpha_h+beta_h);
-
-% Currents
-INa=m(1)^3*gNa*h(1)*(Vm(1)-ENa);
-IK=n(1)^4*gK*(Vm(1)-EK);
-IL=gL*(Vm(1)-EL);
-I_ion=I-IK-INa-IL;     
-
-% Derivatives
-dVm_dt= I_ion/Cm;
-dm_dt=alpha_m*(1-m)-beta_m*m;
-dn_dt=alpha_n*(1-n)-beta_n*n;
-dh_dt=alpha_h*(1-h)-beta_h*h;
+h(1)=alpha_h./(alpha_h+beta_h); 
 
 for i=1:length(t)-1
     
-    m(i+1)=m(i)+dt.*dm_dt;
-    n(i+1)=n(i)+dt.*dn_dt;
-    h(i+1)=m(i)+dt.*dh_dt;
-    
+    % Calculating the new alpha and beta values for m/n/h
     alpha_m=.1*((25-Vm(i))/(exp((25-Vm(i))/10)-1));
     beta_m=4*exp(-Vm(i)/18);
     alpha_n=.01*((10-Vm(i))/(exp((10-Vm(i))/10)-1));
     beta_n=.125*exp(-Vm(i)/80);
     alpha_h=.07*exp(-Vm(i)/20);
     beta_h=1./(exp((30-Vm(i))/10)+1);
+    
+    % Difference between the current m/n/h value and the next one
+    dm_dt=alpha_m*(1-m(i))-beta_m*m(i);
+    dn_dt=alpha_n*(1-n(i))-beta_n*n(i);
+    dh_dt=alpha_h*(1-h(i))-beta_h*h(i);
+
+    % Euler method to find the next m/n/h value    
+    m(i+1)=m(i)+dt.*dm_dt;
+    n(i+1)=n(i)+dt.*dn_dt;
+    h(i+1)=m(i)+dt.*dh_dt;
 
     % Currents
     INa=m(i)^3*gNa*h(i)*(Vm(i)-ENa);
     IK=n(i)^4*gK*(Vm(i)-EK);
     IL=gL*(Vm(i)-EL);
     I_ion=I-IK-INa-IL;     
-
     
+    % Derivative for membrane voltage and Euler method to find the next
+    % voltage value
+    dVm_dt= I_ion/Cm;
     Vm(i+1)=Vm(i)+dt*dVm_dt;
+
 end
 
-plot(t,Vm,'g',t,gK,'b')
+plot(t,Vm)
